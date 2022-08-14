bc53835f4cd301f69259cb8dd3b4be55545b776d
von MUTINDA <maxwellwambua@gmail.com>
2022-08-14 12:37:12 +0300

    bump up deps

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from statistics import mode
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-import pandas as pd
-from sklearn.externals import joblib
-from sklearn.preprocessing import StandardScaler
+import pandas as pd 
+from joblib import dump, load
+from sklearn import svm
+
+from sklearn.preprocessing import StandardScaler 
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -67,5 +70,10 @@ def predict():
 
 if __name__ == "__main__":
     # load pretrained model as clf
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
+    clf = svm.SVC()
+    model_path = "./model_data/boston_housing_prediction.joblib"
+
+    dump(clf, model_path) 
+    clf = load(model_path) 
+
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
